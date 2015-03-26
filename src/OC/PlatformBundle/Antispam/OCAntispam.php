cd7c7d51778615e827d449a8266a87f29f294d0c
dauclem <cedric.roustan@gmail.com>
2015-03-26 19:37:46 +0100

    security end

--- src/OC/PlatformBundle/Antispam/OCAntispam.php
+++ src/OC/PlatformBundle/Antispam/OCAntispam.php
@@ -9,18 +9,19 @@
 namespace OC\PlatformBundle\Antispam;
 
 
-class OCAntispam {
+class OCAntispam extends \Twig_Extension {
 	private $mailer;
 	private $locale;
 	private $minLength;
 
-	public function __construct(\Swift_Mailer $mailer, $locale, $minLength)
-	{
+	public function __construct(\Swift_Mailer $mailer, $minLength) {
 		$this->mailer    = $mailer;
-		$this->locale    = $locale;
-		$this->minLength = (int) $minLength;
+		$this->minLength = (int)$minLength;
 	}
 
+	public function setLocale($locale) {
+		$this->locale = $locale;
+	}
 
 	/**
 	 * Vérifie si le texte est un spam ou non
@@ -28,8 +29,18 @@ class OCAntispam {
 	 * @param string $text
 	 * @return bool
 	 */
-	public function isSpam($text)
-	{
+	public function isSpam($text) {
 		return strlen($text) < $this->minLength;
+	}
+
+	public function getFunctions() {
+		return array(
+			'checkIfSpam' => new \Twig_Function_Method($this, 'isSpam')
+		);
+	}
+
+	// La méthode getName() identifie votre extension Twig, elle est obligatoire
+	public function getName() {
+		return 'OCAntispam';
 	}
 }
